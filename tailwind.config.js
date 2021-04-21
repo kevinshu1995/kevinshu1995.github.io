ca8ddbdd67d8ecbdd21495a1da654c8b66133455
kevin_hws <kevinshu1995@gmail.com>
2021-04-22 00:29:57 +0800

    style: 💄 tailwind color theme & plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	jit: true,
 	purge: {
@@ -11,10 +13,23 @@ module.exports = {
 	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		colors: {
+			primaryYellow: '#D79724',
+			primaryBlue: '#205058',
+			'primaryBlue-dark': '#0A272C',
+			transparent: 'transparent',
+			current: 'currentColor',
+			gray: colors.gray,
+			red: colors.red,
+			yellow: colors.yellow,
+			green: colors.green,
+			black: colors.black,
+			white: colors.white,
+		},
 		extend: {},
 	},
 	variants: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/line-clamp')],
 }
