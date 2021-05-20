e667dcc2e412da8d416cc46492b6618b08873dde
kevin_hws <kevinshu1995@gmail.com>
2021-05-20 10:34:41 +0800

    feat: 🎸 landing layout

--- components/page/index/Landing.vue
+++ components/page/index/Landing.vue
@@ -3,33 +3,33 @@
 		id="landing"
 		class="h-screen grid grid-flow-col grid-cols-12 grid-rows-9 select-none relative"
 	>
-		<div class="absolute left-0 top-0 w-full h-full">
+		<div class="absolute z-30 left-0 top-0 w-full h-full">
 			<div
 				class="grid grid-flow-col grid-cols-12 grid-rows-9 h-full relative"
 			>
 				<div
-					class="row-start-2 md:row-start-2 row-end-9 md:row-end-9 col-start-1 md:col-start-1 col-end-13 md:col-end-6 lg:col-end-6 xl:col-end-5 flex items-end justify-end z-10"
+					class="row-start-2 md:row-start-2 row-end-10 sm:row-end-9 md:row-end-9 col-start-1 md:col-start-1 col-end-13 md:col-end-6 lg:col-end-6 xl:col-end-5 flex items-end justify-end z-10"
 				>
 					<div class="h-full md:h-5/6">
 						<img
 							class="h-full w-auto object-cover object-right"
-							src="~images/me.png"
+							src="~images/me_2.png"
 							alt="Kevin's Picture"
 						/>
 					</div>
 				</div>
 				<div
-					class="row-start-5 row-end-9 col-start-1 col-end-13 relative z-20"
+					class="row-start-5 row-end-10 sm:row-end-9 col-start-1 col-end-13 relative z-20"
 				>
 					<div
 						class="absolute z-20 left-0 bottom-0 w-full h-full bg-gradient-to-b from-transparent to-primaryBlue-500 bg-opacity-70 md:hidden"
 					></div>
 				</div>
 				<div
-					class="row-start-1 row-end-9 col-start-2 md:col-start-6 lg:col-start-6 xl:col-start-5 col-end-13 flex flex-col items-start justify-end text-white filter drop-shadow space-y-2 md:space-y-4 pl-3 pb-5 z-30"
+					class="row-start-1 row-end-9 sm:row-end-8 col-start-2 md:col-start-6 lg:col-start-6 xl:col-start-5 col-end-13 flex flex-col items-start justify-end text-white filter drop-shadow space-y-2 md:space-y-4 pl-3 pb-5 z-30"
 				>
 					<h1
-						class="font-black flex flex-col text-8xl xs:text-9xl sm:text-9xl md:text-11xl lg:text-15xl 2xl:text-19xl leading-none -ml-1 sm:-ml-2 md:-ml-2.5 lg:-ml-4 xl:-ml-5"
+						class="font-black flex flex-col text-8xl xs:text-9xl sm:text-9xl md:text-10xl lg:text-13xl 2xl:text-17xl leading-none -ml-1 sm:-ml-2 md:-ml-2.5 lg:-ml-4 xl:-ml-5"
 					>
 						<span
 							class="transform translate-y-3 md:translate-y-5 lg:translate-y-10"
@@ -49,8 +49,12 @@
 					>
 						我是許文修，目前是一名網頁設計師，曾經是平面設計師，目前致力於研究前端技術，寫網頁齁勝！
 					</p>
-					<div class="-mx-2">
-						<div class="flex items-center space-x-3">
+				</div>
+				<div
+					class="row-start-9 sm:row-start-8 row-end-10 sm:row-end-9 col-start-2 md:col-start-6 lg:col-start-6 xl:col-start-5 col-end-13 flex flex-col items-start justify-end text-white filter drop-shadow space-y-2 md:space-y-4 pl-3 z-30"
+				>
+					<div class="-mx-2 h-full">
+						<div class="flex items-center space-x-3 h-full">
 							<template v-for="link in socialLinks">
 								<navLink
 									:key="`social-${link.icon}`"
@@ -72,10 +76,10 @@
 			class="row-start-1 row-end-9 col-start-1 md:col-end-4 bg-primaryYellow-500"
 		></div> -->
 		<div
-			class="row-start-1 row-end-9 md:row-end-8 col-start-1 md:col-start-4 col-end-13 bg-gradient-to-r to-primaryBlue-500 from-primaryBlue-dark-500"
+			class="relative z-20 row-start-1 row-end-10 sm:row-end-9 md:row-end-8 col-start-1 md:col-start-4 col-end-13 bg-gradient-to-r to-primaryBlue-500 from-primaryBlue-dark-500"
 		></div>
 		<div
-			class="row-start-1 md:row-start-9 row-end-9 md:row-end-6 col-start-1 col-end-7 sm:col-end-8 md:col-end-13 lg:col-end-12 bg-gradient-to-l to-primaryYellow-500 from-primaryYellow-400"
+			class="relative z-10 row-start-1 md:row-start-9 row-end-10 sm:row-end-9 md:row-end-7 xl:row-end-6 col-start-1 col-end-7 sm:col-end-8 md:col-end-13 lg:col-end-12 bg-gradient-to-l to-primaryYellow-500 from-primaryYellow-400"
 		></div>
 	</div>
 </template>
