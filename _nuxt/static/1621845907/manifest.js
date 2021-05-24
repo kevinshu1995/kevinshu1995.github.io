86807c3a67d63f2ef236c5a78b9f1052466de3d6
kevinshu1995 <kevinshu1995@users.noreply.github.com>
2021-05-24 08:45:11 +0000

    deploy: c453c366da865e97af188251c39ab19614531549

---
+++ _nuxt/static/1621845907/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fdesign\u002FThuCoolCard","\u002Fdesign\u002FCzjhTengoPoster","\u002Fdesign\u002Fbanners","\u002Fdesign\u002FFoodSciencePoster","\u002Fdesign\u002FSoarSportPoster","\u002Fdesign\u002Fswing"]})
