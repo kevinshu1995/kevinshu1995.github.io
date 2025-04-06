95faade30ef4c86a08e5438eeb26f4d419cd1348
kevinhws <kevinshu1995@gmail.com>
2025-04-07 01:54:11 +0800

    feat: v3 first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/scripts'
+  ]
+})
