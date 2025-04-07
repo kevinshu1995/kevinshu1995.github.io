5f541c9be9d22b8a322215f9f2bb7fdfa2ac45b2
kevinhws <kevinshu1995@gmail.com>
2025-04-08 01:13:55 +0800

    feat(resume): 新增簡歷顯示與資料結構定義
    - 新增 HomeLanding 組件以顯示簡歷狀態
    - 更新首頁以包含 HomeLanding 組件
    - 新增簡歷資料結構定義

---
+++ components/home/HomeLanding.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>{{ resumeState }}</div>
+</template>
+
+<script setup lang="ts">
+const resumeState = useState("resume")
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-    <div>
-        <h1 class="font-900 text-12">Hello 你好</h1>
-        <p class="font-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            quas dolor officia expedita odit quidem quasi dolore exercitationem
-            rem labore id possimus minima at mollitia totam aspernatur quae,
-            nesciunt distinctio.
-        </p>
+    <div class="container mx-auto px-4">
+        <div class="-mx-4 grid grid-cols-4 gap-4">
+            <div class="col-start-2 grid-col-span-2">
+                <HomeLanding />
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import type { Resume } from "@/types/resume"
+const runtimeConfig = useRuntimeConfig()
+const { en, zh_TW } = runtimeConfig.public.resume
+
+const resumeState = useState<{ [key: string]: Resume | null }>(
+    "resume",
+    () => ({
+        en: null,
+        tw: null,
+    }),
+)
+
+await callOnce(async () => {
+    const { data } = await useFetch<string>(en)
+    if (data.value) resumeState.value.en = JSON.parse(data.value) as Resume
+})
+
+await callOnce(async () => {
+    const { data } = await useFetch<string>(zh_TW)
+    if (data.value) resumeState.value.tw = JSON.parse(data.value) as Resume
+})
+</script>
