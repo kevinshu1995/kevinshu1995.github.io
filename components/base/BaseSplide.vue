ccc933ce5fe932f71b055679c62669af2847f16b
kevinhws <kevinshu1995@gmail.com>
2025-04-12 16:47:45 +0800

    feat(splide): 新增 Splide 組件以支援輪播功能
    - 新增 BaseSplide.vue 組件，實現基本的輪播效果
    - 更新 package.json 和 package-lock.json，加入 @splidejs/splide 依賴

---
+++ components/base/BaseSplide.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <section
+            class="splide"
+            aria-label="Splide Basic HTML Example"
+            ref="splide"
+        >
+            <div class="splide__track">
+                <ul class="splide__list">
+                    <li class="splide__slide" v-for="slide in slides">
+                        <slot name="slide" :slide="slide"></slot>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Splide, { type Options } from "@splidejs/splide"
+import "@splidejs/splide/css"
+
+const props = defineProps<{
+    slides: any[]
+    options: Options
+}>()
+
+const splide = ref(null)
+
+onMounted(() => {
+    if (!splide.value) {
+        console.error("Splide element not found")
+        return
+    }
+    new Splide(splide.value, props.options).mount()
+})
+</script>
