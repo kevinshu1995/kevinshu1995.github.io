6a3a6a6f00de9241e9cde7f881961e027a95a2a2
kevinhws <kevinshu1995@gmail.com>
2025-04-26 17:36:55 +0800

    feat(footer): 更新頁腳樣式與新增功能
    - 調整頁腳背景顏色與文字顏色
    - 新增「回到頂部」按鈕功能
    - 更新社交媒體連結顯示
    - 新增聯絡描述文字

--- components/base/BaseFooter.vue
+++ components/base/BaseFooter.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="mt-30 bg-neutral-100">
-        <div class="container mx-auto px-4 h-full pt-10 space-y-20">
-            <div class="grid md:grid-cols-2 gap-4">
-                <div></div>
-                <div class="space-y-8">
-                    <div class="flex gap-4 border-b border-b-neutral-200 pb-2">
+    <div class="mt-30 bg-neutral-900 text-neutral-200">
+        <div
+            class="container mx-auto px-4 h-full py-36 space-y-20 md:space-y-30"
+        >
+            <div class="grid md:grid-cols-2 gap-12 md:gap-4">
+                <div class="space-y-4 md:space-y-8 order-2 md:order-1">
+                    <div class="grayscale-100">
+                        <img
+                            src="https://i.giphy.com/JIX9t2j0ZTN9S.webp"
+                            alt=""
+                            class="size-20 object-contain rounded-full"
+                        />
+                    </div>
+                    <p class="text-3 whitespace-pre-line">
+                        {{ t("contact.description") }}
+                    </p>
+                    <div class="flex gap-4 pb-2">
                         <div class="flex flex-col">
                             <div class="flex justify-end items-end gap-2">
                                 <a
@@ -29,12 +40,27 @@
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="flex items-start order-1 md:order-2">
+                    <button
+                        class="flex gap-2 items-center font-bold text-4 group cursor-pointer"
+                        @click="scrollToTop"
+                        :data-mouse-event="$t('mouseEvent.scrollToTop')"
+                    >
+                        <Icon
+                            name="mdi:arrow-up-thin"
+                            class="size-6 group-hover:animate-bounce"
+                        />
+                        <span>Back to top</span>
+                    </button>
+                </div>
+            </div>
+            <div class="grid md:grid-cols-4 gap-x-4 gap-y-8 md:gap-y-18">
+                <div class="space-y-4 md:col-start-3">
+                    <h6 class="text-3 text-neutral-500">出沒地帶</h6>
                     <div class="space-y-4">
                         <ul class="flex flex-col justify-end flex-wrap gap-4">
-                            <li
-                                v-for="social in socials"
-                                class="border-b border-b-neutral-200 pb-1"
-                            >
+                            <li v-for="social in socials">
                                 <a
                                     :href="social.url"
                                     class="flex gap-1 items-center h-full w-full hover:underline group"
@@ -46,7 +72,7 @@
                                     <span class="flex gap-3 items-center">
                                         <Icon
                                             :name="`simple-icons:${social.network}`"
-                                            class="size-5"
+                                            class="size-4"
                                         />
 
                                         <span class="capitalize text-3">
@@ -62,23 +88,33 @@
                         </ul>
                     </div>
                 </div>
+                <div class="space-y-4 md:col-start-3">
+                    <h6 class="text-3 text-neutral-500">地點</h6>
+                    <div class="flex gap-1">
+                        <p>台灣</p>
+                        <span>/</span>
+                        <p>TAIWAN</p>
+                    </div>
+                </div>
+                <div class="space-y-4">
+                    <p class="text-3 text-neutral-500">.</p>
+                    <p class="flex items-center gap-1 text-3">
+                        <span>©</span>
+                        <span class="">{{ new Date().getFullYear() }}</span>
+                        <span>{{ nameEn }}</span>
+                        <span class="">{{ nameTw }}</span>
+                    </p>
+                </div>
             </div>
-
-            <p
-                class="flex items-center gap-1 text-3 py-4 border-t border-t-neutral-200"
-            >
-                <span>©</span>
-                <span class="">{{ new Date().getFullYear() }}</span>
-                <span>{{ nameEn }}</span>
-                <span class="">{{ nameTw }}</span>
-            </p>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { useClipboard, useTimeoutFn } from "@vueuse/core"
+import { useClipboard, useTimeoutFn, useWindowScroll } from "@vueuse/core"
 import type { ResumeState } from "@/types/state"
+
+const { y: windowY } = useWindowScroll({ behavior: "smooth" })
 
 const { locale, t } = useI18n()
 
@@ -108,5 +144,9 @@ function copyEmail() {
     copy()
     copyEmailMouseEventText.value = t("mouseEvent.copied")
     start()
+}
+
+function scrollToTop() {
+    windowY.value = 0
 }
 </script>
